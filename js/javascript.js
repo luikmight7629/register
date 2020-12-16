f7269858431d6d08d9f4f41bd0fbc765b698cc79
luikmight7629 <71313255+luikmight7629@users.noreply.github.com>
2020-12-16 12:50:34 +0800

    Add files via upload

---
+++ js/javascript.js
@@ -0,0 +1,38 @@
+function checkblank(){
+ var errormessager="";
+if (document.getElementById('fname').value == "") {
+	errormessager+="Please Enter your first name \n";
+}
+if (document.getElementById('mname').value == "") {
+	errormessager+="Please Enter your middle name \n";
+}
+if (document.getElementById('lname').value == "") {
+	errormessager+="Please Enter your lastname \n";
+}
+if (document.getElementById('cn').value == "") {
+	errormessager+="Please Enter contact number \n";
+}
+if (document.getElementById('cn').value <=11) {
+	errormessager+="Enter 11 numbers \n";
+}
+if (document.getElementById('ea').value =="") {
+	errormessager+="Enter email Address \n";
+}
+if (document.getElementById('user').value =="") {
+	errormessager+="Enter User Name \n";
+}
+if (document.getElementById('pass').value =="") {
+	errormessager+="Enter password \n";
+}
+if (document.getElementById('pass').value >=4) {
+	errormessager+="Enter atleast 5 \n";
+}
+if (document.getElementById('pass').value !=document.getElementById('Cpass').value) {
+	errormessager+="atleast havethe same password \n";
+}
+
+if (errormessager != "") {
+	alert(errormessager);
+	return false;
+}
+	}	
